e697e9a73cf51caf0c0dd0e79636e0642165dd3c
Ganesh <fartadeganesh16@gmail.com>
2025-03-07 17:59:25 +0530

    Bank-App-Java

---
+++ src/main/java/in/sp/main/GaneshBankAppApplication.java
@@ -0,0 +1,13 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaneshBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaneshBankAppApplication.class, args);
+	}
+
+}
